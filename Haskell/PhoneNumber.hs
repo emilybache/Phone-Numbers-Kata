5f9f71f14fe19ddd5fe7441d988bbae29b51a061
= <jeff.foster@acm.org>
2013-08-09 12:06:47 +0100

    start looking at this from a Haskell perspective

---
+++ Haskell/PhoneNumber.hs
@@ -0,0 +1,12 @@
+module PhoneNumber where
+
+data Digit = Number Int
+           | Space
+           | Dash deriving (Show,Eq)	   
+
+data TelephoneNumber = TelephoneNumber [Digit] deriving (Show,Eq)
+
+data AddressBook = AddressBook [TelephoneNumber]
+
+isConsistent :: AddressBook -> Bool
+isConsistent (AddressBook numbers) = undefined 
