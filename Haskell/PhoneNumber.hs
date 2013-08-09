069b6267f38296c2a3f035cc94b149b14179e23c
= <jeff.foster@acm.org>
2013-08-09 12:20:50 +0100

    sketch out top level

--- Haskell/PhoneNumber.hs
+++ Haskell/PhoneNumber.hs
@@ -12,11 +12,18 @@ data Digit = Number Int
 instance Arbitrary Digit where
   arbitrary = elements (Space : Dash : digits) where
     digits = map Number [0..9]
-    
 
 data TelephoneNumber = TelephoneNumber [Digit] deriving (Show,Eq)
+
+instance Arbitrary TelephoneNumber where  
+  arbitrary = do
+    nums <- listOf1 arbitrary
+    return (TelephoneNumber nums)
 
 data AddressBook = AddressBook [TelephoneNumber]
 
 isConsistent :: AddressBook -> Bool
-isConsistent (AddressBook numbers) = undefined 
+isConsistent (AddressBook numbers) = not (any id (map (isPrefixOf numbers) numbers))
+
+isPrefixOf :: [TelephoneNumber] -> TelephoneNumber -> Bool
+isPrefixOf = undefined
