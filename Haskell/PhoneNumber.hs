aede4145332550574c63d9ad0dc1df99101a045d
= <jeff.foster@acm.org>
2013-08-09 12:53:47 +0100

    demonstrate

--- Haskell/PhoneNumber.hs
+++ Haskell/PhoneNumber.hs
@@ -19,16 +19,17 @@ instance Arbitrary Digit where
   arbitrary = elements (Space : Dash : digits) where
     digits = map Number [0..9]
 
-data TelephoneNumber = TelephoneNumber [Digit] deriving (Eq)
+data TelephoneNumber = TelephoneNumber String [Digit] deriving (Eq)
 
 instance Show TelephoneNumber where
-  show (TelephoneNumber ds) = concatMap show ds
+  show (TelephoneNumber name ds) = name ++ " : " ++ concatMap show ds
 
 instance Arbitrary TelephoneNumber where  
   arbitrary = do
     num <- elements (map Number [0..9]) -- Must begin with number
     nums <- listOf1 arbitrary
-    return (TelephoneNumber (num:nums))
+    name <- elements ["John", "Fred", "George"]
+    return (TelephoneNumber name (num:nums))
 
 data AddressBook = AddressBook [TelephoneNumber] deriving Show
 
@@ -45,9 +46,17 @@ isPrefixOf nums num = any (startsWith num)  nums
 
 -- Deliberately hyper naive!
 startsWith :: TelephoneNumber -> TelephoneNumber -> Bool
-startsWith (TelephoneNumber a) (TelephoneNumber b) = all id (zipWith (==) (filter isNumber a) (filter isNumber b))
+startsWith (TelephoneNumber _ a) (TelephoneNumber _ b) = all id (zipWith (==) (filter isNumber a) (filter isNumber b))
 
 isNumber :: Digit -> Bool
 isNumber (Number _) = True
 isNumber _          = False
+
+-- Now, with this naive definition we can create test data
+
+inconsistent :: IO [AddressBook]
+inconsistent = sample' (suchThat arbitrary (not . isConsistent))
+
+consistent :: IO [AddressBook]
+consistent = sample' (suchThat arbitrary isConsistent)
 
