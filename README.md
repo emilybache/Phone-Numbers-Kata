0c3e89f3224f0687a18c07e39a7935349d8f117c
emilybache <emily@bacheconsulting.com>
2013-08-09 11:58:46 +0200

    initial import

---
+++ README.md
@@ -0,0 +1,23 @@
+# Phone Numbers Code Kata
+
+This repo contains some sample data you could use to help test your solution to the Phone Numbers code kata. It also contains the python script I used to create the sample data. You can tweak it to create data that suits your needs.
+
+The Names.csv file is from ["Generating Random Names as Test Data"](http://www.markrichman.com/2007/09/26/generating-random-names-as-test-data/), and that page says it is adapted from US census data. I have just converted his file from excel to csv to make it easier to handle.
+
+# Kata description
+
+_this description is taken from [http://cyber-dojo.com](http://cyber-dojo.com)_
+
+Given a list of phone numbers, determine if it is 
+consistent. In a consistent phone list no number 
+is a prefix of another. For example:
+
+- Bob 91 12 54 26
+- Alice 97 625 992
+- Emergency 911
+
+In this case, it is not possible to call Bob because 
+the phone exchange would direct your call to the 
+emergency line as soon as you dialled the first three 
+digits of Bob's phone number. So this list would not 
+be consistent.
---
+++ generate_data.py
@@ -0,0 +1,44 @@
+"""
+Use this script to generate a file of names and phone numbers suitable for load testing the Phone Numbers code kata.
+"""
+
+import csv
+import os
+from random import choice, sample, randrange
+
+phone_number_digits = ["", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+phone_number_partitions = ["", "-", " "]
+
+def random_phonenumber():
+    number = "0{0}{1}{2}{3}{4}{5}{6}".format("".join(sample(phone_number_digits, randrange(2,6))), 
+                                    "".join(sample(phone_number_partitions, 1)), 
+                                    "".join(sample(phone_number_digits, 3)),
+                                    "".join(sample(phone_number_partitions, 1)),
+                                    "".join(sample(phone_number_digits, 2)),
+                                    "".join(sample(phone_number_partitions, 1)),
+                                    "".join(sample(phone_number_digits, 2)),
+                                    )
+    return number
+
+
+def name_generator(names_file):
+    names_source = csv.reader(names_file)
+    for row in names_source:
+        yield("{0} {1}".format(row[randrange(0,2)], row[2]))
+    
+
+def generate_phonenumbers(outfile, name_source, entry_count=1000):
+    writer = csv.writer(outfile)
+    writer.writerow(["Name", "Phone Number"])
+    for i, name in zip(range(entry_count), name_source):
+        number = random_phonenumber()
+        writer.writerow([name, number])   
+
+if __name__ == "__main__":
+    entry_count = 1000 # edit this to get a bigger file
+    with open("phone_data.txt", "w") as f:
+        with open("Names.csv", encoding="UTF-8", newline='') as names_file:
+            name_source = name_generator(names_file)
+            generate_phonenumbers(f, name_source, entry_count)
+
+        
