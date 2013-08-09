c8b3d07723e8869c1d999312e4559a8c4beaabf9
= <jeff.foster@acm.org>
2013-08-09 12:09:40 +0100

    define an arbitrary element

--- Haskell/PhoneNumber.hs
+++ Haskell/PhoneNumber.hs
@@ -2,9 +2,17 @@ module PhoneNumber where
 
 import Test.QuickCheck
 
+maxLength :: Int
+maxLength = 15
+
 data Digit = Number Int
            | Space
            | Dash deriving (Show,Eq)	   
+
+instance Arbitrary Digit where
+  arbitrary = elements (Space : Dash : digits) where
+    digits = map Number [0..9]
+    
 
 data TelephoneNumber = TelephoneNumber [Digit] deriving (Show,Eq)
 
